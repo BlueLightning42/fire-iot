eb939e4abceaf64b65e57580fd522f3c7924f639
Blue Ϟ Lightning <jawg45@live.com>
2020-04-09 21:27:15 -0400

    create psudocode for the device

---
+++ device/psudocode.c
@@ -0,0 +1,28 @@
+#include "LowPower.h"
+
+struct Packet{
+  uint16_t id;
+  byte type;
+  byte state;
+  //etc
+}
+#define Value int
+
+void setup(){
+  char* info = getInfo();
+  packet = makePacket();
+  send(setup);
+  send(info);
+  id = response();
+}
+
+// main loop
+void loop(){
+  Value value = readValue();
+  // in practice the following is completly dependant on the protocol used to communicate with the gateway
+  Packet packet = makePacket(value);
+  sendPacket(packet);
+  
+  // Sleep for 8 s with ADC module and BOD module off
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+}
